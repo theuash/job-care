e29c3f38808dc1c85566704609dfbcf412274be6
theuash@gmail.com <theuash@gmail.com>
2026-07-20 06:29:42 +0530

    backend

---
+++ backend/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("java")
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "com.example.backend.BackendApplicationKt"
+    }
+}
