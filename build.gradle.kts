b90d84d67d8e034fe64af0b19114f7c775f4fc25
theuash@gmail.com <theuash@gmail.com>
2026-07-20 06:30:03 +0530

    note

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.android.application") version "8.4.2" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24" apply false
+    id("org.springframework.boot") version "3.3.2" apply false
+    id("io.spring.dependency-management") version "1.1.6" apply false
+}
